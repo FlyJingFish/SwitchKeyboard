9c5d1b77aa109448509df30e2e2cca8ccd7dcd26
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2023-05-17 11:33:42 +0800

    1，更新demo

--- app/src/main/java/com/flyjingfish/switchkeyboard/ExampleDialogActivity.java
+++ app/src/main/java/com/flyjingfish/switchkeyboard/ExampleDialogActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.flyjingfish.switchkeyboard.databinding.ActivityExampleDialogBinding;
+import com.flyjingfish.switchkeyboardlib.SwitchKeyboardUtil;
 
 public class ExampleDialogActivity extends AppCompatActivity {
     private ActivityExampleDialogBinding binding;
@@ -14,7 +15,11 @@ public class ExampleDialogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityExampleDialogBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        StatusBarHelper.transparencyBar(this);
+
+        //请注意这块也是必须做的，不然首次打开DialogFragment时你会看到状态栏被改变了
+        SwitchKeyboardUtil switchKeyboardUtil = new SwitchKeyboardUtil(this);
+        switchKeyboardUtil.setSystemUi();
+
         binding.llMsgContent.setOnClickListener(v -> {
             InputDialog inputDialog = InputDialog.getDialog(binding.etContent.getText().toString());
             inputDialog.setOnContentCallBack(new InputDialog.OnContentCallBack() {
